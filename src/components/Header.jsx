92a579e3b770240e2a5aac29db39de01e10d30ce
Guilherme David <guilhermedavidrk@icloud.com>
2022-08-13 20:08:24 -0300

    :hammer: create header

---
+++ src/components/Header.jsx
@@ -0,0 +1,18 @@
+import style from '../styles/Header.module.css'
+import iconIgnite from '../assets/logo-ignite.svg'
+
+
+export function Header() {
+    return (
+        <header>
+            <div className={style.headerTop}>
+                <div className={style.img}>
+                    <img src={iconIgnite} />
+                </div>
+                <div className={style.text}>
+                    <p>Ignite Feed</p>
+                </div>
+            </div>
+        </header>
+    )
+}
