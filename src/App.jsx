bd6f172004ec01dda0fe29694a643528378677bb
Guilherme David <guilhermedavidrk@icloud.com>
2022-08-14 15:57:37 -0300

    :hammer: create sidebar

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,19 @@
 import { Header } from './components/Header'
+import { Sidebar } from './components/Sidebar'
 
+import styles from './App.module.css'
 import './global.css'
 
 export function App() {
   return (
-    <Header />
+    <>
+
+      <Header />
+
+      <div className={styles.wrapper}>
+        <Sidebar />
+      </div>
+
+    </>
   )
 }
--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -4,15 +4,17 @@ import iconIgnite from '../assets/logo-ignite.svg'
 
 export function Header() {
     return (
-        <header>
-            <div className={style.headerTop}>
-                <div className={style.img}>
-                    <img src={iconIgnite} />
+        <>
+            <header>
+                <div className={style.headerTop}>
+                    <div className={style.img}>
+                        <img src={iconIgnite} />
+                    </div>
+                    <div className={style.text}>
+                        <p>Ignite Feed</p>
+                    </div>
                 </div>
-                <div className={style.text}>
-                    <p>Ignite Feed</p>
-                </div>
-            </div>
-        </header>
+            </header>
+        </>
     )
 }
