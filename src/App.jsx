e07dc246c93210e6c49548415eca41a35b61bd16
Guilherme David <guilhermedavidrk@icloud.com>
2022-08-16 19:41:18 -0300

    :zap: adding initial functionality

--- src/App.jsx
+++ src/App.jsx
@@ -5,6 +5,64 @@ import { Post } from './components/Post'
 import styles from './App.module.css'
 import './global.css'
 
+
+const posts = [
+  {
+    id: 1,
+    author: {
+      avatarUrl: 'https://github.com/manogui777.png',
+      name: 'Guilherme David',
+      role: 'Student ReactJS',
+    },
+    content: [
+      {
+        type: 'paragraph',
+        content: 'Fala galeraa 👋'
+      },
+      {
+        type: 'paragraph',
+        content: 'Acabei de subir mais um projeto no meu portifa. É um projeto que fiz no NLW Return, evento da Rocketseat. O nome do projeto é DoctorCare 🚀',
+      },
+      {
+        type: 'link',
+        content: '👉jane.design/doctorcare',
+      },
+      {
+        type: 'link-hashTag',
+        content: ['#novoprojeto', '#ReactJS', '#nlw'],
+      },
+    ],
+    publishedAt: new Date('2022-08-03 20:00:00'),
+  },
+  {
+    id: 2,
+    author: {
+      avatarUrl: 'https://github.com/maykbrito.png',
+      name: 'Mayk Brito',
+      role: 'Educator @rocketseat',
+    },
+    content: [
+      {
+        type: 'paragraph',
+        content: 'Fala galeraa 👋',
+      },
+      {
+        type: 'paragraph',
+        content: 'Acabei de subir mais um projeto no meu portifa. É um projeto que fiz no NLW Return, evento da Rocketseat. O nome do projeto é DoctorCare 🚀',
+      },
+      {
+        type: 'link',
+        content: '👉jane.design/doctorcare',
+      },
+      {
+        type: 'link-hashTag',
+        content: ['#novoprojeto', '#ReactJS', '#nlw'],
+      },
+    ],
+    publishedAt: new Date('2022-08-10 20:30:00'),
+  },
+];
+
 export function App() {
   return (
     <>
@@ -13,11 +71,17 @@ export function App() {
 
       <div className={styles.wrapper}>
         <Sidebar />
-        
+
         <main>
-          <Post />
-          <Post />
-          <Post />
+          {posts.map(posts => {
+            return (
+              <Post
+                author={posts.author} 
+                content={posts.content} 
+                publishedAt={posts.publishedAt}
+              />
+            )
+          })}
         </main>
       </div>
     </>
--- src/components/Post.jsx
+++ src/components/Post.jsx
@@ -1,34 +1,52 @@
 import styles from "../styles/Post.module.css";
+
+import { format, formatDistanceToNow } from 'date-fns'
+import ptBR from 'date-fns/locale/pt-BR'
+
 import { Avatar } from "./Avatar";
 import { Comment } from "./Comment";
+import { PaintBrushBroad } from "phosphor-react";
+import { pt } from "date-fns/locale";
 
-export function Post() {
+export function Post(props) {
+
+const publishedDateFormated = format(props.publishedAt, "d 'de' LLLL 'às' HH:mm'h'", {
+  locale: ptBR,
+});
+
+const publishedDateRelative = formatDistanceToNow(props.publishedAt, {
+  locale: ptBR,
+  addSuffix: true,
+})
+
   return (
     <article className={styles.post}>
       <header>
 
         <div className={styles.author}>
-          <Avatar /*hasBorder={true}*/ src="https://github.com/maykbrito.png" />
+
+          <Avatar /*hasBorder={true}*/ src={props.author.avatarUrl} />
+
           <div className={styles.authorInfo}>
-            <strong>Guilherme David</strong>
-            <span>Dev Front End</span>
+            <strong>{props.author.name}</strong>
+            <span>{props.author.role}</span>
           </div>
+
         </div>
-        <time dateTime="Publicado em 14/08/2022">Publicado ha 1h</time>
+
+        <time title={publishedDateFormated} dateTime={props.publishedAt.toISOString()}>{publishedDateRelative}</time>
+
       </header>
 
       <div className={styles.content}>
-        <p>Fala galeraa 👋</p>
-
-        <p>Acabei de subir mais um projeto no meu portifa. É um projeto que fiz no NLW Return, evento da Rocketseat. O nome do projeto é DoctorCare 🚀</p>
-
-        <p><a href="#">👉jane.design/doctorcare</a></p>
-
-        <p>
-          <a href="#">#novoprojeto</a>{'  '}
-          <a href="#">#nlw</a>{'  '}
-          <a href="#">#rocketseat</a>{'  '}
-        </p>
+        {props.content.map(line => {
+          if (line.type === 'paragraph') {
+            return (<p>{line.content}</p>)
+          } else if (line.type === 'link'){
+            return(<p><a href="#">{line.content}</a></p>)
+          } else {
+            return(<p><a href="#">{line.content}</a></p>)
+          }})}
       </div>
 
       <form className={styles.comentForm}>
